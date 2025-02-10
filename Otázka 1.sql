5a77f3153b58d89718d0f6b502de1717d6889b6e
Petr <hlisnikovskyp@gmail.com>
2025-02-10 18:39:43 +0100

    Add files via upload

---
+++ Otázka 1.sql
@@ -0,0 +1,37 @@
+
+-- Vývoj mezd ve sledovaném časovém období za jednotlivá průmyslová odvětví
+
+SELECT
+    odvetvi,
+    rok_mzdy AS rok,
+    prumerna_rocni_mzda AS prumerna_mzda,
+    prumerna_rocni_mzda - LAG(prumerna_rocni_mzda) OVER (PARTITION BY odvetvi ORDER BY rok_mzdy) AS mezirocni_zmena
+FROM 
+	t_petr_hlisnikovsky_project_sql_primary_final
+WHERE 
+	odvetvi IS NOT NULL
+ORDER BY 
+	odvetvi, 
+	rok;
+
+
+-- Roky kdy došlo k meziročnímu poklesu mezd v jednotlivých průmyslových odvětích 
+
+WITH mzdy AS (
+    SELECT
+        odvetvi,
+        rok_mzdy AS rok,
+        prumerna_rocni_mzda AS prumerna_mzda,
+        prumerna_rocni_mzda - LAG(prumerna_rocni_mzda) OVER (PARTITION BY odvetvi ORDER BY rok_mzdy) AS mezirocni_zmena
+    FROM 
+    	t_petr_hlisnikovsky_project_sql_primary_final
+)
+SELECT 
+	* 
+FROM 
+	mzdy
+WHERE 
+	mezirocni_zmena < 0
+ORDER BY 
+	odvetvi, 
+	rok;
